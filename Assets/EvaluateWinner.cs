937c0b291b3e8d837fa491acaa9e7c1651eecc40
Alex Nguyen <amn7417@mavs.uta.edu>
2024-12-02 17:02:33 -0600

    <chore>: Removed previous merge comments

--- Assets/EvaluateWinner.cs
+++ Assets/EvaluateWinner.cs
@@ -2,28 +2,16 @@ using UnityEngine;
 
 public class EvaluateWinner : MonoBehaviour
 {
-<<<<<<< HEAD
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-=======
-    
 
->>>>>>> 6d6dd2321a30e4ff2741fa1f53b8fee24f17436d
     void Start()
     {
         
     }
 
-<<<<<<< HEAD
-    // Update is called once per frame
-=======
->>>>>>> 6d6dd2321a30e4ff2741fa1f53b8fee24f17436d
+
     void Update()
     {
         
     }
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 6d6dd2321a30e4ff2741fa1f53b8fee24f17436d
 }
