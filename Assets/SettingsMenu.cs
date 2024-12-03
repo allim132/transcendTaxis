ce556906251b55b26c714be574074aa5bb17baff
Steffanie Mock <sdm7526@mavs.uta.edu>
2024-12-02 20:43:17 -0600

    settings added

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -1,27 +1,80 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
-    public void SetQuality(int qualityIndex)
-    {
-        QualitySettings.SetQualityLevel(qualityIndex); // Apply the selected quality level
-        PlayerPrefs.SetInt("QualitySetting", qualityIndex); // Save the setting
-        PlayerPrefs.Save();
-    }
+    public AudioMixer audioMixer; // Reference to the Audio Mixer
+    public Slider masVol; // Master volume slider
+    public Slider musVol; // Music volume slider
+    public Slider sfxVol; // SFX volume slider
+    public TMP_Dropdown qualityDropdown; // Quality dropdown
+    public Slider brightnessSlider; // Brightness slider
 
     private void Start()
     {
-        // Load the saved quality level at the start of the game
-        if (PlayerPrefs.HasKey("QualitySetting"))
+        // Load saved quality level
+        int savedQuality = PlayerPrefs.GetInt("QualitySetting", QualitySettings.GetQualityLevel());
+        QualitySettings.SetQualityLevel(savedQuality);
+        if (qualityDropdown != null)
         {
-            int savedQuality = PlayerPrefs.GetInt("QualitySetting");
-            QualitySettings.SetQualityLevel(savedQuality);
+            qualityDropdown.value = savedQuality;
         }
+
+        // Load saved volumes
+        masVol.value = PlayerPrefs.GetFloat("masterVolume", 0.75f);
+        musVol.value = PlayerPrefs.GetFloat("musicVolume", 0.75f);
+        sfxVol.value = PlayerPrefs.GetFloat("SFXVolume", 0.75f);
+
+        SetMasterVolume(masVol.value);
+        SetMusicVolume(musVol.value);
+        SetSFXVolume(sfxVol.value);
+
+        // Load brightness
+        float savedBrightness = PlayerPrefs.GetFloat("Brightness", 1.0f);
+        brightnessSlider.value = savedBrightness;
+        AdjustBrightness(savedBrightness);
+    }
+
+    public void SetQualityLevel(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+        PlayerPrefs.SetInt("QualitySetting", qualityIndex);
+        PlayerPrefs.Save(); // Save changes
+    }
+
+    public void AdjustBrightness(float value)
+    {
+        RenderSettings.ambientLight = Color.white * value;
+        PlayerPrefs.SetFloat("Brightness", value);
+        PlayerPrefs.Save(); // Save changes
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+        PlayerPrefs.SetFloat("masterVolume", volume);
+        PlayerPrefs.Save(); // Save changes
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+        PlayerPrefs.Save(); // Save changes
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+        PlayerPrefs.Save(); // Save changes
     }
 
     public void Exit()
     {
-        SceneManager.LoadScene("mainMenuScreen");
+        SceneManager.LoadScene("mainMenuScreen"); // Load the main menu scene
     }
 }
