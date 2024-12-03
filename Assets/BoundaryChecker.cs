c276e4ba3ad0a10745f4227ec0aa81793005efa3
Alex Nguyen <amn7417@mavs.uta.edu>
2024-12-02 22:47:03 -0600

    <feat>: Taxi has bounds check to reset position

---
+++ Assets/BoundaryChecker.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class BoundaryChecker : MonoBehaviour
+{
+    public float minX, maxX, minY, maxY, minZ, maxZ;
+    private Transform taxiTransform;
+
+    void Start()
+    {
+        taxiTransform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        Vector3 position = taxiTransform.position;
+        bool isOutOfBounds = position.x < minX || position.x > maxX ||
+                             position.y < minY || position.y > maxY ||
+                             position.z < minZ || position.z > maxZ;
+
+        if (isOutOfBounds)
+        {
+            HandleOutOfBounds();
+        }
+    }
+
+    void HandleOutOfBounds()
+    {
+        // Reset the taxi's position or implement game over logic
+        taxiTransform.position = new Vector3(-80f, 1f, 12f); // Example reset position
+        Debug.Log("Taxi out of bounds!");
+    }
+}
